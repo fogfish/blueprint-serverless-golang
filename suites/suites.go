5cae91075b9aa34f70ad822e22ee51750153a7ba
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-03-07 22:41:10 +0200

    update testing suites

--- suites/suites.go
+++ suites/suites.go
@@ -1,19 +1,15 @@
 package suites
 
 import (
-	"os"
-
 	"github.com/fogfish/gurl/v2/http"
 	ƒ "github.com/fogfish/gurl/v2/http/recv"
 	ø "github.com/fogfish/gurl/v2/http/send"
 )
 
-var host = os.Getenv("INPUT_TARGET")
-
 // TestNews endpoint
 func TestNews() http.Arrow {
 	return http.GET(
-		ø.URI(host+"/scud"),
+		ø.URI("/scud"),
 		ƒ.Status.OK,
 	)
 }
